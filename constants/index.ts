9e3ce1a1eea8da901573c410ddd396c131d21fb4
ShinYoung-Kim <lukey0515@naver.com>
2024-04-02 20:07:57 +0900

    feature: side bar 상수 설정

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+	{
+		label: "Home",
+		route: "/",
+		imagUrl: "/icons/Home.svg",
+	},
+	{
+		label: "Upcoming",
+		route: "/upcoming",
+		imagUrl: "/icons/upcoming.svg",
+	},
+	{
+		label: "Previous",
+		route: "/previous",
+		imagUrl: "/icons/previous.svg",
+	},
+	{
+		label: "Recordings",
+		route: "/recordings",
+		imagUrl: "/icons/Video.svg",
+	},
+	{
+		label: "Personal",
+		route: "/personal-room",
+		imageUrl: "/icons/add-personal.svg",
+	},
+];
